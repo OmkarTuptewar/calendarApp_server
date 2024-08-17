6a22ba6331520399a779a3d3812316f128cc174a
OmkarTuptewar <omkarsanjay2004@gmail.com>
2024-08-18 02:47:42 +0530

    fixed deploy issue

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const cors = require('cors'); 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-const filePath = path.join(__dirname, '..', 'public', 'database.json');
+const filePath = path.join(__dirname, 'database.json');
 
 const readEventsFromFile = () => {
   if (!fs.existsSync(filePath)) {
